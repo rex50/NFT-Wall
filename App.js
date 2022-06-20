e59ea325cdc0ac058f8ab3ad72fcf6a2053cfb67
Pavitra Raut <pareshraut50@gmail.com>
2022-06-20 15:05:23 +0530

    Loaded custom fonts

--- App.js
+++ App.js
@@ -19,6 +19,17 @@ const screenOptions = {
 }
 
 const App = () => {
+
+  const [loaded] = useFonts({
+    InterBold: require("./assets/fonts/Inter-Bold.ttf"),
+    InterSemiBold: require("./assets/fonts/Inter-SemiBold.ttf"),
+    InterMedium: require("./assets/fonts/Inter-Medium.ttf"),
+    InterRegular: require("./assets/fonts/Inter-Regular.ttf"),
+    InterLight: require("./assets/fonts/Inter-Light.ttf"),
+  });
+
+  if(!loaded) return null;
+
   return (
     <NavigationContainer theme = {theme}>
       <Stack.Navigator screenOptions={screenOptions} initialRouteName="Home">
